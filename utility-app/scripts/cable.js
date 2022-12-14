85c38c168d74fe25603e7caa04873eaad7ca97f0
Okorie GoodGod <guddytechs@gmail.com>
2022-12-14 12:44:21 +0100

    added the js for the fund wallet

--- utility-app/scripts/cable.js
+++ utility-app/scripts/cable.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const button = document.querySelector('.showbutton');
     const subcriptionBox = document.querySelector(".purchase-collapse");
 
+    const button2 = document.querySelector('.li-fund');
+    const subcriptionBox2 = document.querySelector(".fund-collapse");
+
     button.addEventListener('click', function(e){
         e.preventDefault();
         subcriptionBox.classList.toggle('hide')
+
+    })
+
+    button2.addEventListener('click', function(e){
+        e.preventDefault();
+        subcriptionBox2.classList.toggle('hide2')
 
     })
 
